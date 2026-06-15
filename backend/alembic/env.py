ed0944e62b3fe645b6c4677201ba8272e2d64d2a
Henry Chukwunwike Dibie <99669374+HenryMorganDibie@users.noreply.github.com>
2026-06-15 01:03:16 +0100

    Add statement_cache_size to connect_args

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -44,7 +44,8 @@ async def run_migrations_online():
         config.get_section(config.config_ini_section),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
-    )
+        connect_args={"statement_cache_size": 0},
+)
     async with connectable.connect() as connection:
         await connection.run_sync(do_run_migrations)
     await connectable.dispose()
